5f4faecdb6b983b2b1dc0d34f135255b9e297349
beathindidubbed-lab <beathindidubbed@gmail.com>
2025-12-25 13:39:57 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -6,42 +6,54 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 
 from bot import Bot
-from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
-from helper_func import encode
+from config import OWNER_ID, ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
+from helper_func import encode, shorten_url
 
-@Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
+@Bot.on_message(filters.private & filters.user([OWNER_ID] + ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats','setup','help','custom_batch','range_help']))
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("Please Wait...!", quote = True)
+    """Handle private messages from admins to create shareable links"""
+    reply_text = await message.reply_text("Please Wait...!", quote=True)
     try:
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
     except FloodWait as e:
-        await asyncio.sleep(e.value)
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+        await asyncio.sleep(e.x)
+        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
     except Exception as e:
         print(e)
         await reply_text.edit_text("Something went Wrong..!")
         return
+    
     converted_id = post_message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
+    
+    # Try to shorten URL
+    shortened_link = await shorten_url(link)
+    
+    reply_markup = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={shortened_link}')]
+    ])
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-
-    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(
+        f"<b>Here is your link</b>\n\n{shortened_link}",
+        reply_markup=reply_markup,
+        disable_web_page_preview=True
+    )
 
     if not DISABLE_CHANNEL_BUTTON:
         try:
             await post_message.edit_reply_markup(reply_markup)
         except FloodWait as e:
-            await asyncio.sleep(e.value)
+            await asyncio.sleep(e.x)
             await post_message.edit_reply_markup(reply_markup)
         except Exception:
             pass
 
 @Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
 async def new_post(client: Client, message: Message):
-
+    """Automatically add share button to channel posts"""
+    
     if DISABLE_CHANNEL_BUTTON:
         return
 
@@ -49,11 +61,18 @@ async def new_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    
+    # Try to shorten URL
+    shortened_link = await shorten_url(link)
+    
+    reply_markup = InlineKeyboardMarkup([
+        [InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={shortened_link}')]
+    ])
+    
     try:
         await message.edit_reply_markup(reply_markup)
     except FloodWait as e:
-        await asyncio.sleep(e.value)
+        await asyncio.sleep(e.x)
         await message.edit_reply_markup(reply_markup)
     except Exception:
         pass
